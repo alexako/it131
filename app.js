b5c351a846cf6a44b7d059bf7a9aa271715bb279
Alex <akosialexpo@gmail.com>
2017-10-15 23:13:52 +0800

    Remove angular and fix request header

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ var connection = mysql.createConnection({
 });
 var app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('views', path.join(__dirname, 'views'));
