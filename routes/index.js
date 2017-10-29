aeb02bcc39c974f062b206258f5725f491a10b46
Alex <akosialexpo@gmail.com>
2017-10-30 00:10:23 +0800

    Add delete method and refactor

--- routes/index.js
+++ routes/index.js
@@ -12,15 +12,17 @@ var connection = mysql.createConnection({
 
 var api = {
   getAll:  function(req, res) {
-          connection.query('SELECT * FROM bars LEFT JOIN address ON bars.address_id = address.id', function(err, result) {
+          connection.query('SELECT bars.id, bars.name, description, address_id, street, city, province, zip, country FROM bars LEFT JOIN address ON bars.address_id = address.id', function(err, result) {
             if (err) throw err;
             res.send(result);
+            //connection.end();
           });
         },
   getBarByID: function(req, res) {
-          connection.query('SELECT * FROM bars LEFT JOIN address ON bars.address_id = address.id WHERE bars.id = ' + req.params.id, function(err, result) {
+          connection.query('SELECT bars.id, bars.name, description, address_id, street, city, province, zip, country FROM bars LEFT JOIN address ON bars.address_id = address.id WHERE bars.id = ' + req.params.id, function(err, result) {
             if (err) throw err;
             res.send(result);
+            //connection.end();
           }) 
         },
   add: function(req, res) {
@@ -47,7 +49,6 @@ var api = {
                   + address_id + ", '"
                   + req.body[0].description
                   + "');";
-                console.log(bar_query);
 
                 connection.query(bar_query, function(err, result) {
                     if (err) {
@@ -62,29 +63,71 @@ var api = {
                         })
                       }
                       res.send(result);
-                      connection.end();
+                      //connection.end();
                     });
                 });
             });
           });
         },
   edit: function(req, res) {
-          console.log("edit req:", req);
           connection.query("INSERT INTO bars(name, rating, address_id, description) VALUE ('" + req.body[0].name + "', '" + req.body[0].rating + "', '" + addressResult.insertId + "', '" + req.body[0].description + "')", function(err, result) { 
             if (err) throw err;
             res.send(result);
+            //connection.end();
           })
         },
   delete: function(req, res) {
-          console.log("delete req:", req);
+          connection.beginTransaction(function(err) {
+
+            var address_query = "SELECT address_id FROM bars WHERE bars.id = "
+              + req.params.id + ";";
+            connection.query(address_query, function(err, result) {
+                if (err) {
+                  connection.rollback(function() {
+                    throw err;
+                  })
+                }
+
+                console.log(result);
+                var address_id = result[0].address_id;
+                if (!address_id) return;
+
+                var bar_query = "DELETE FROM bars WHERE id = "
+                  + req.params.id + ";";
+                connection.query(bar_query, function(err, result) {
+                    if (err) {
+                      connection.rollback(function() {
+                        throw err;
+                      })
+                    }
+                    connection.commit(function(err) {
+                      if (err) {
+                        connection.rollback(function() {
+                          throw err;
+                        })
+                      }
+                      res.send(result);
+                      //connection.end();
+                    });
+                });
+                
+                connection.query("DELETE FROM address WHERE id = " + address_id, function(err, result) {
+                  if (err) {
+                    connection.rollback(function() {
+                      throw err;
+                    })
+                  }
+                });
+            });
+          });
         }
 }
 
 router.get('/', api.getAll);
 router.get('/:id', api.getBarByID)
-router.post('/add/', api.add);
-router.put('/edit/:id', api.edit);
-router.delete('/delete/:id', api.delete);
+router.post('/', api.add);
+router.put('/:id', api.edit);
+router.delete('/:id', api.delete);
 
 
 module.exports = router;
