6cf20e39be2a548e8ab537d8813f2ca8376b74ba
Alex <akosialexpo@gmail.com>
2017-10-29 16:50:35 +0800

    Refactor post method

--- routes/index.js
+++ routes/index.js
@@ -12,30 +12,68 @@ var connection = mysql.createConnection({
 
 var api = {
   getAll:  function(req, res) {
-          connection.query('SELECT * FROM bars', function(err, result) {
+          connection.query('SELECT * FROM bars LEFT JOIN address ON bars.address_id = address.id', function(err, result) {
             if (err) throw err;
             res.send(result);
           });
         },
   getBarByID: function(req, res) {
-          connection.query('SELECT * FROM bars WHERE id = ' + req.params.id, function(err, result) {
+          connection.query('SELECT * FROM bars LEFT JOIN address ON bars.address_id = address.id WHERE bars.id = ' + req.params.id, function(err, result) {
             if (err) throw err;
             res.send(result);
           }) 
         },
   add: function(req, res) {
-          console.log("add params:", req.body);
-          var address_id;
-          connection.query("INSERT INTO address(street, city, province, zip, country) VALUES ('" + req.body[0].street + "', '" + req.body[0].city + "', '" + req.body[0].province + "', '" + req.body[0].zip + "', '" + req.body[0].country + "')", function(err, addressResult) {
-              if (err) throw err;
-              connection.query("INSERT INTO bars(name, rating, address_id, description) VALUE ('" + req.body[0].name + "', '" + req.body[0].rating + "', '" + addressResult.insertId + "', '" + req.body[0].description + "')", function(err, result) { 
-                if (err) throw err;
-                res.send(result);
-              })
+          connection.beginTransaction(function(err) {
+            var address_query = "INSERT INTO address(street, city, province, zip, country) VALUE ('"
+              + req.body[0].street + "', '"
+              + req.body[0].city + "', '"
+              + req.body[0].province + "', '"
+              + req.body[0].zip + "', '"
+              + req.body[0].country + "');";
+
+            connection.query(address_query, function(err, result) {
+                if (err) {
+                  connection.rollback(function() {
+                    throw err;
+                  })
+                }
+
+                var address_id = result.insertId;
+
+                var bar_query = "INSERT INTO bars(name, rating, address_id, description) VALUE ('"
+                  + req.body[0].name + "', '"
+                  + req.body[0].rating + "', "
+                  + address_id + ", '"
+                  + req.body[0].description
+                  + "');";
+                console.log(bar_query);
+
+                connection.query(bar_query, function(err, result) {
+                    if (err) {
+                      connection.rollback(function() {
+                        throw err;
+                      })
+                    }
+                    connection.commit(function(err) {
+                      if (err) {
+                        connection.rollback(function() {
+                          throw err;
+                        })
+                      }
+                      res.send(result);
+                      connection.end();
+                    });
+                });
+            });
           });
         },
   edit: function(req, res) {
           console.log("edit req:", req);
+          connection.query("INSERT INTO bars(name, rating, address_id, description) VALUE ('" + req.body[0].name + "', '" + req.body[0].rating + "', '" + addressResult.insertId + "', '" + req.body[0].description + "')", function(err, result) { 
+            if (err) throw err;
+            res.send(result);
+          })
         },
   delete: function(req, res) {
           console.log("delete req:", req);
